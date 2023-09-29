f5ab7945b01cecdc090350b0944c099139cc01fc
Emanuel Bodin <embodin@kth.se>
2023-09-29 14:43:25 +0200

    updated readme

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,6 @@ const defaultBlocksConfig = {
   image: '1.77257',
   footer: '1.202278',
   search: '1.77262',
-  /*  language: {
-    en: '1.77273',
-    sv: '1.272446',
-  }, */
   klaroConfig: '1.1137647',
   matomoAnalytics: '1.714097',
 }
